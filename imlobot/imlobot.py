1dc50b8d51da7457a5325fcac4a0c24bbd9c5c42
Amirjoin <amirjonkarimoff@gmail.com>
2023-10-05 07:58:50 +0500

    add shablon with mohirdev

--- imlobot/imlobot.py
+++ imlobot/imlobot.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from test import checkWord
-API_TOKEN = '6473410449:AAE6FTD8Vg0Bm36Xx5UxcP0oRWf994mTM2Q'
+API_TOKEN = '6285949027:AAGYKt_z9OWmxmVGVywslHKEG_ZOWtVm6ck'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,15 +19,16 @@ async def userhelp(message: types.Message):
 
 @dp.message_handler()
 async def checkimlo(message: types.Message):
-    word = message.text
-    result = checkWord(word)
-    if result['available']:
-        response += f"{word.capitalize()}"
-    else:
-        response = f"xato:{word.capitalize()}n"
-        for text in result['matches']:
-            response += f"tugri:{text.capitalize()}"
-    await message.answer(response)
-
+    words = message.text.split()
+    for word in words:
+        result = checkWord(word)
+        if result['available']:
+            response = f"{word.capitalize()}"
+        else:
+            response = f"xato:{word.capitalize()}\n"
+            for text in result['matches']:
+                response += f"tugri:{text.capitalize()}\n"
+        await message.answer(response)
+    
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
